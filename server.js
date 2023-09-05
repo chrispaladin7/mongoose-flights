71e26ad43ded9cd19b9b12e290ac36cca21a388f
chrispaladin7 <chrismartey@yahoo.com>
2023-09-05 18:47:29 -0400

    Adds node,express and mongoose dependences

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// add mongoose process
+require('dotenv').config();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
